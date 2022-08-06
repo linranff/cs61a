9a8fba0b2a30329daabd23b6b0b01c665ae08aa4
linran <linranff@gmail.com>
2022-08-06 14:09:15 +0800

    lab00 finished
    lab00 finished

--- lab/lab00/lab00.py
+++ lab/lab00/lab00.py
@@ -1,3 +1,6 @@
+from http.client import OK
+
+
 def twenty_twenty_two():
     """Come up with the most creative expression that evaluates to 2022,
     using only numbers and the +, *, and - operators.
@@ -5,4 +8,5 @@ def twenty_twenty_two():
     >>> twenty_twenty_two()
     2022
     """
-    return ______
+    
+    return 2000 + 30 - 16 // 2
